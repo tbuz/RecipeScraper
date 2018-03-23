0321fa911a701f98ebdba05f22a19ddaf537bea3
Tolga Buz <buz_tolga@yahoo.de>
2018-03-23 11:16:51 +0100

    added data, images, word embeddings and fasttext notebook

---
+++ data/get_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://www.dropbox.com/s/ik0rkg1o6jlwdxy/recipe_data.zip?dl=0 -O data.zip
+unzip data.zip
+
---
+++ word_embeddings/get_embeddings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget  -O embeddings.zip
+unzip embeddings.zip
+
