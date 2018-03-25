9c03afca9bbf9b7cc9f0cd0c4859562f875b57e4
tbuz <buz_tolga@yahoo.de>
2018-03-25 21:44:43 +0200

    updated notebook files and get_data script

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/ik0rkg1o6jlwdxy/recipe_data.zip?dl=0 -O data.zip
+wget https://www.dropbox.com/s/kxkk61idhlpa1ml/data.zip?dl=0 -O data.zip
 unzip data.zip
